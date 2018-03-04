a2ca7693228a50ac6a48489948ea7366e751d688
Antonio Reid <read.musik@gmail.com>
2018-03-04 23:42:05 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,9 +26,10 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series 
+  "SELECT series.title FROM series
    JOIN characters
-   ON characters.series_id = series.id;"
+   ON characters.series_id = series.id
+   GROUP BY MAX(COUNT(characters.species));"
 end
 
 def select_character_names_and_number_of_books_they_are_in
