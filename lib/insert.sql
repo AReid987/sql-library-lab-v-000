024e57cf020343423dbb7637ff66a67528b4925b
Antonio Reid <read.musik@gmail.com>
2018-03-04 21:39:54 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,10 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("game of thrones", 1, 3);
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("ender's game", 2, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("A Song of Ice and Fire", 1, 1);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Ender's Game", 2, 2);
+
+
+INSERT INTO books (title, year, series_id) VALUES ("A Game of Thrones", 1996, 1);
+INSERT INTO books (title, year, series_id) VALUES ("A Clash of Kings", 1999, 1);
+INSERT INTO books (title, year, series_id) VALUES ("A Storm of Swords", 2000, 1);
+INSERT INTO books (title, year, series_id) VALUES ("Ender's Game", 1985, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Speaker For the Dead", 1986, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Xenocide", 1991, 2);
