62696d6e77df126ab2384c90b8d0d82f6b9bc954
Antonio Reid <read.musik@gmail.com>
2018-03-04 21:57:14 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,3 +8,12 @@ INSERT INTO books (title, year, series_id) VALUES ("A Storm of Swords", 2000, 1)
 INSERT INTO books (title, year, series_id) VALUES ("Ender's Game", 1985, 2);
 INSERT INTO books (title, year, series_id) VALUES ("Speaker For the Dead", 1986, 2);
 INSERT INTO books (title, year, series_id) VALUES ("Xenocide", 1991, 2);
+
+INSERT INTO characters (name, motto, species, series_id, author_id) VALUES ("Daenerys Targaryen", "Mother of Dragons", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, series_id, author_id) VALUES ("Tyrion Lannister", "A Lannister always pays his debts", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, series_id, author_id) VALUES ("Jon Snow", "Winter is coming", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, series_id, author_id) VALUES ("Arya Stark", "Winter is coming", "Human", 1, 1);
+INSERT INTO characters (name, motto, species, series_id, author_id) VALUES ("Ender", "I'll beat you unfairly", "Human", 2, 2);
+INSERT INTO characters (name, motto, species, series_id, author_id) VALUES ("Valentine", "Sometimes lies were more dependable than the truth.", "Human", 2, 2);
+INSERT INTO characters (name, motto, species, series_id, author_id) VALUES ("Colonel Graff", "Humanity does not ask us to be happy.", "Human", 2, 2);
+INSERT INTO characters (name, motto, species, series_id, author_id) VALUES ("Bean", "Early to bed and early to rise", "Human", 2, 2);
