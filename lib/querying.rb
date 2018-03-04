f7e679d5fb04797215df0830b799602b41f1fb0b
Antonio Reid <read.musik@gmail.com>
2018-03-04 23:45:05 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,8 +29,8 @@ def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
    JOIN characters
    ON characters.series_id = series.id
-   ORDER BY COUNT(characters.species)
-   ;"
+   GROUP BY characters.species
+   HAVING MAX(characters.species);"
 end
 
 def select_character_names_and_number_of_books_they_are_in
